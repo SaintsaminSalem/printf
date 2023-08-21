789b1e8a925b8f016bf92581534c3211b1e83996
SaintsaminSalem <ekariasalem1@gmail.com>
2023-08-21 09:31:58 -0700

    main.c

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 /**
  * main - Entry point
  *
- * .......
  * Return: Always 0
  */
 int main(void)
@@ -14,7 +13,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
-
+	
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
